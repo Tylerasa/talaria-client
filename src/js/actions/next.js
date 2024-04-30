60ea72126f8598b0a2615e2fb1e6a8457efa08c2
sylvester-tiacloud <sylvester.sarpong@tiacloud.io>
2024-04-30 14:11:08 +0000

    feat: handling build errors

--- src/js/actions/next.js
+++ src/js/actions/next.js
@@ -3,9 +3,20 @@ export function isNext() {
   getErrorFile()
     .then((result) => {
       if (result !== "Element not found") {
-        const parts = result.split(" ");
-        const file = parts[0];
-        let line = getLineChar(result);
+        let file, line;
+        if (result.startsWith("./")) {
+          let text = result.substring(2) 
+          let firstColonIndex = text.indexOf(":");
+
+          file = text.substring(0, firstColonIndex);
+
+          line = text.substring(firstColonIndex + 1);
+        } else {
+          const parts = result.split(" ");
+          file = parts[0];
+          line = getLineChar(result);
+        }
+
         errors.push({
           file,
           line,
@@ -31,37 +42,55 @@ function getLineChar(text) {
   }
   return "";
 }
+
 function getErrorFile() {
   const elementWithTitle = document.querySelector("nextjs-portal");
 
-  if (elementWithTitle) {
-    const shadowRoot = elementWithTitle.shadowRoot;
+  if (!elementWithTitle) {
+    return Promise.resolve("Element not found");
+  }
 
-    return new Promise((resolve) => {
-      const observer = new MutationObserver(function (mutations) {
-        for (const mutation of mutations) {
-          if (mutation.type === "childList") {
-            for (const node of mutation.addedNodes) {
-              const el = node.querySelector(
-                '[title="Click to open in your editor"]'
-              );
-              if (el) {
-                const spanElement = el.querySelector("span");
+  const shadowRoot = elementWithTitle.shadowRoot;
 
-                if (spanElement) {
-                  observer.disconnect();
-                  resolve(spanElement.textContent);
-                  return;
-                }
-              }
+  if (!shadowRoot) {
+    return Promise.resolve("Shadow root not found");
+  }
+
+  const buildErrorElement = shadowRoot.querySelector(
+    "#nextjs__container_errors_label"
+  );
+
+  if (
+    buildErrorElement &&
+    buildErrorElement.innerText.includes("Build Error")
+  ) {
+    const errorFileElement = shadowRoot.querySelector("[role=link]");
+    if (errorFileElement) {
+      const filePath = errorFileElement.textContent;
+      return Promise.resolve(
+        filePath
+      );
+    }
+  }
+
+  return new Promise((resolve) => {
+    const observer = new MutationObserver((mutations) => {
+      for (const mutation of mutations) {
+        if (mutation.type === "childList") {
+          for (const node of mutation.addedNodes) {
+            const errorFileElement = node.querySelector(
+              '[title="Click to open in your editor"] span, [title="Click to open in your editor"] div'
+            );
+            if (errorFileElement) {
+              observer.disconnect();
+              resolve(errorFileElement.textContent);
+              return;
             }
           }
         }
-      });
-
-      observer.observe(shadowRoot, { childList: true });
+      }
     });
-  } else {
-    return Promise.resolve("Element not found");
-  }
+
+    observer.observe(shadowRoot, { childList: true, subtree: true });
+  });
 }
